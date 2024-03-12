10f807ec06ba7eda9c91646a8d5e96daa5edcebd
JuanSMarin <juansmarin269@gmail.com>
2024-03-12 12:02:12 -0500

    Juan Sebastian 1

---
+++ Assets/Juego/Scripts/UI/buttons.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttons : MonoBehaviour
+{
+public void iniciar()
+{
+SceneManager.LoadScene("Nivel 1");
+Debug.Log("Jugar precionado");
+}
+
+public void creditos()
+{
+SceneManager.LoadScene("Creditos");  
+Debug.Log("creditos precionado");
+}
+
+public void instrucciones()
+{
+SceneManager.LoadScene("Instrucciones");    
+Debug.Log("instrucciones precionado");
+}
+
+public void volverMenu()
+{
+SceneManager.LoadScene("MenuPrincipal");
+Debug.Log("volver al menu precionado");
+}
+
+public void salir()
+{
+Application.Quit();
+Debug.Log("salir precionado");
+}
+
+
+}
