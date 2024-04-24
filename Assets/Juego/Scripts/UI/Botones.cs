750014b4f51aafe919ce7f6be5aee8def6d4cce2
B09S105est <B09S105est@MEDAULAS.UPB>
2024-04-24 07:45:21 -0500

    Game over

---
+++ Assets/Juego/Scripts/UI/Botones.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Botones : MonoBehaviour
+{
+
+
+    public void BotonIniciar()
+    {
+        Debug.Log("Boton precionado");
+    }
+    public void BotonSalir()
+    {
+        Debug.Log("Boton precionado");
+    }
+    public void BotonCreditos()
+    {
+        Debug.Log("Boton precionado");
+    }
+    public void BotonInstrucciones()
+    {
+        Debug.Log("Boton precionado");
+    }
+    public void BotonVolverMenu()
+    {
+        Debug.Log("Boton precionado");
+    }
+}
