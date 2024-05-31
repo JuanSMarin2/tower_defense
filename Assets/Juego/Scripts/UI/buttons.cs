e3addb301da4ac10f1247fe1ee5cdaf235fa7e90
danielfmonsalve <monsalved632@gmail.com>
2024-05-31 12:45:56 -0500

    CambiosScriptButtons

--- Assets/Juego/Scripts/UI/buttons.cs
+++ Assets/Juego/Scripts/UI/buttons.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class buttons : MonoBehaviour
+public class buttons : MonoBehaviour //Scrip botones
 {
 public void iniciar()
 {
